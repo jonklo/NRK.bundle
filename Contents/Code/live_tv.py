0fb0020db7ee17c344f694ac5987ca77cd9ad399
jonklo <jonklo@gmail.com>
2012-06-16 13:31:11 +0200

    Temporary fix for live TV streams.

--- Contents/Code/live_tv.py
+++ Contents/Code/live_tv.py
@@ -3,19 +3,22 @@
 LIVE_TV_STATIONS = (
     {
         'title': 'NRK 1', 
-        'url': 'mms://straumv.nrk.no/nrk_tv_direkte_nrk1_%s',
+        #'url': 'http://nrk1-i.akamaihd.net/hls/live/201543/nrk1/master.m3u8',
+        'url': 'http://nrk1-i.akamaihd.net/hls/live/201543/nrk1/master_Layer3.m3u8',
         'desc': u'Bredt og variert programtilbud. Norges største tv-kanal.', 
         'img': 'nrk1.png'
     },
     {
         'title': 'NRK 2', 
-        'url': 'mms://straumv.nrk.no/nrk_tv_direkte_nrk2_%s',
+        #'url': 'http://nrk2-i.akamaihd.net/hls/live/201544/nrk2/master.m3u8',
+        'url': 'http://nrk2-i.akamaihd.net/hls/live/201544/nrk2/master_Layer3.m3u8',
         'desc': u'Fordypningskanalen. Bakgrunns-, dokumentar og nyhetskanal.', 
         'img': 'nrk2.png'
     }, 
     {
         'title': 'NRK Super / NRK 3', 
-        'url': 'mms://straumv.nrk.no/nrk_tv_direkte_nrk3_%s',
+        #'url': 'http://nrk3-i.akamaihd.net/hls/live/201545/nrk3/master.m3u8',
+        'url': 'http://nrk3-i.akamaihd.net/hls/live/201545/nrk3/master_Layer3.m3u8',
         'desc': u'Den tredje kanalen tilbyr vekselsvis et barnetilbud og et tilbud for unge voksne med serier, humor film.', 
         'img': 'nrk3.png'
     }
@@ -29,9 +32,9 @@ def LiveTVMenu(sender):
     dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
     
     for station in LIVE_TV_STATIONS:
-        url = station['url'] % Prefs['livetv_quality']
+        url = station['url']
         Log('Added %s' % url)
         
-        dir.Append(WindowsMediaVideoItem(url, title=station['title'], summary=station['desc'], thumb=R('nrk-nett-tv.png'), width=768, height=432)) # TODO thumb=R(station['img'])
+        dir.Append(VideoItem(url, title=station['title'], summary=station['desc'], thumb=R('nrk-nett-tv.png'))) 
     
     return dir
